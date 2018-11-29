3d64766a805088e636d1ecc236f5d3e71675f48c
shuangsong <shuangsong6@gmail.com>
2018-11-29 10:12:05 -0800

    note 2-11.29

--- note_2.py
+++ note_2.py
@@ -41,11 +41,29 @@ def translate(phrase):
     translation  = ""
     for letter in phrase:
         if letter in "AEIOUaeiou":
-            translation = translation + "g"
+        #or if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
         else:
             translation = translation +letter
     return translation
 
 #test translation
 print(translate(input("Enter a phrase:")))
+
+'''
+you can type comment in here in another way beside #
+'''
+
+
+
+#except command: really powerful thing protect program from create errors.
+try:
+    value = 10/0
+    number = int(input("enter a number:"))
+    print(number)
+except :
+    print("invalid input")
 
