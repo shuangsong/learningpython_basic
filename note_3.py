3587d49221d1c89e45be33bb87e2e99e222ae13c
shuangsong <shuangsong6@gmail.com>
2018-11-29 10:30:18 -0800

    note 3-11.29

---
+++ note_3.py
@@ -0,0 +1,17 @@
+#Reading from external files:
+employee_file = open("exployees.txt", "") #if file and your python file in the same directory.
+
+#read mode could be:
+'''
+"r" mode --read file
+"w" mode -- write file , change 
+"a" mode -- append file, add new information to the file
+"r+" mode -- read and write file
+'''
+employee_file.close() #after you read the file you can close.
+
+
+
+
+
+
